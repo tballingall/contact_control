c361c2110bf3e1ec307854132ef348f95482f3c4
Tom Ballingall <tom.ballingall@gmail.com>
2016-12-17 18:04:32 -0500

    Placeholder commit. Compant, User, & registration models have minimal methods created with explanation. The idea atm is that none should be doing more than they need to.

---
+++ app/models/directory.rb
@@ -0,0 +1,17 @@
+#might be too much. This is a JIC model
+
+class Directory
+
+  def collected_information
+    #this would be data storage/retrieval
+    #based on  params
+  end
+
+  def complete_retrieval
+    #retrieves  all information
+  end
+
+  def selective_rertieval
+    #retrieves users or corp based on params
+  end
+end
---
+++ app/models/partner.rb
@@ -0,0 +1,15 @@
+#This should be a simple model that mainly deals
+#with routing
+class Partner
+  def partner
+    #unsure, this seems like a possible crud
+    #issue but more lika a field on user/corp
+    #you pay money, you are a partner
+    #you get priveleges
+  end
+
+  def partner_privileges
+    # When parameters are met (did you give us money)
+    # partner will have access to an admin panel
+  end
+end
