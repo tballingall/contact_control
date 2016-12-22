17e3601ac963ac168ce5606afda049b59373277d
Tom Ballingall <tom.ballingall@gmail.com>
2016-12-21 21:59:45 -0500

    Added services and mehtods applicable to each. This is a checkin commit.

---
+++ app/services/listings.rb
@@ -0,0 +1,15 @@
+#Used for job openinngs or other public announcements
+
+def listing(params);
+end
+
+# An index of lstings by user.
+# requires listings and user_id
+#returns an array
+def user_listings(listings, user_id)
+end
+
+#most viewed listings. Will probababy
+#require more paramters. Place holder
+def top_listings(listings, params)
+end
